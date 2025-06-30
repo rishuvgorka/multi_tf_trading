1af194595a63da84df9e81fb9ed6484522a6284b
rishuvgorka <rishuvgorka.rg@gmail.com>
2025-06-30 13:23:10 +0530

    updated

--- backtest/backtest.py
+++ backtest/backtest.py
@@ -9,12 +9,12 @@ def prepare_data(df_15m, df_1h):
     return df_15m.dropna()
 
 if __name__ == '__main__':
-    df_15m = pd.read_csv('inFiles/15m_data.csv', parse_dates=['timestamp'], index_col='timestamp')
-    df_1h = pd.read_csv('inFiles/1h_data.csv', parse_dates=['timestamp'], index_col='timestamp')
+    df_15m = pd.read_csv('../inFiles/15m_data.csv', parse_dates=['timestamp'], index_col='timestamp')
+    df_1h = pd.read_csv('../inFiles/1h_data.csv', parse_dates=['timestamp'], index_col='timestamp')
 
     df = prepare_data(df_15m, df_1h)
     bt = Backtest(df, MultiTimeframeStrategy, cash=1000000, commission=0.001)
     stats = bt.run()
     bt.plot()
-    stats['_trades'].to_csv("ouFiles/backtest_trades.csv", index=False)
+    stats['_trades'].to_csv("../ouFiles/backtest_trades.csv", index=False)
 
